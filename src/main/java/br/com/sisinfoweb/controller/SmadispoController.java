f9fd28cd330346f8db6d70da8b3bdfd145e93415
Bruno Nogueira Silva <ns.bruno@gmail.com>
2017-09-08 09:40:59 -0400

    Melhorias Diversas

--- src/main/java/br/com/sisinfoweb/controller/SmadispoController.java
+++ src/main/java/br/com/sisinfoweb/controller/SmadispoController.java
@@ -8,7 +8,9 @@ package br.com.sisinfoweb.controller;
 import br.com.sisinfoweb.banco.beans.RetornoWebServiceBeans;
 import br.com.sisinfoweb.banco.beans.StatusRetornoWebServiceBeans;
 import br.com.sisinfoweb.banco.values.MensagemPadrao;
+import br.com.sisinfoweb.entity.CfaclifoEntity;
 import br.com.sisinfoweb.entity.SmadispoEntity;
+import br.com.sisinfoweb.service.CfaclifoService;
 import br.com.sisinfoweb.service.SmadispoService;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
@@ -32,139 +34,171 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @author Bruno
  */
 @Controller
-public class SmadispoController extends BaseMyController{
-    
+public class SmadispoController extends BaseMyController {
+
     @Autowired
     private SmadispoService smadispoService;
-    
+
+    @Autowired
+    private CfaclifoService cfaclifoService;
+
     @RequestMapping(value = {"/Smadispo"}, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
     @Override
-    public String initJson( Model model, 
-                            @RequestHeader() HttpHeaders httpHeaders, 
-                            HttpServletResponse response, 
-                            @RequestParam(name = "dispositivo", required = true) String dispositivo,
-                            @RequestParam(name = "columnSelected", required = false) String columnSelected,
-                            @RequestParam(name = "where", required = false) String where,
-                            @RequestParam(name = "resume", required = false, defaultValue = "false") Boolean resume,
-                            @RequestParam(name = "sqlQuery", required = false) String sqlQuery) {
-        
+    public String initJson(Model model,
+            @RequestHeader() HttpHeaders httpHeaders,
+            HttpServletResponse response,
+            @RequestParam(name = "dispositivo", required = true) String dispositivo,
+            @RequestParam(name = "columnSelected", required = false) String columnSelected,
+            @RequestParam(name = "where", required = false) String where,
+            @RequestParam(name = "resume", required = false, defaultValue = "false") Boolean resume,
+            @RequestParam(name = "sqlQuery", required = false) String sqlQuery) {
+
         StatusRetornoWebServiceBeans statusRetorno = new StatusRetornoWebServiceBeans();
         RetornoWebServiceBeans retornoWebService = new RetornoWebServiceBeans();
-        try{
+        try {
             List<SmadispoEntity> lista;
             // Checa se foi passado alqum parametro para filtrar
-            if ( ((sqlQuery != null) && (!sqlQuery.isEmpty())) || 
-                    ((columnSelected != null) && (!columnSelected.isEmpty())) || 
-                    ((where != null) && (!where.isEmpty())) ){
+            if (((sqlQuery != null) && (!sqlQuery.isEmpty()))
+                    || ((columnSelected != null) && (!columnSelected.isEmpty()))
+                    || ((where != null) && (!where.isEmpty()))) {
                 // Pesquisa de acordo com o sql passado
                 lista = smadispoService.findCustomNativeQuery(resume, sqlQuery, columnSelected, where);
-            
+
             } else {
                 lista = smadispoService.findAll();
             }
             // Cria uma vareavel para retorna o status
             statusRetorno.setCodigoRetorno(HttpURLConnection.HTTP_OK);
             statusRetorno.setMensagemRetorno(String.valueOf(HttpStatus.OK));
-            
+
             // Adiciona o status
             retornoWebService.statusRetorno = statusRetorno;
             // Adiciona os dados que eh pra ser retornado
             retornoWebService.object = lista;
-            
+
             return new Gson().toJson(retornoWebService);
-        } catch(Exception e){
+        } catch (Exception e) {
             // Cria uma vareavel para retorna o status
             statusRetorno.setCodigoRetorno(HttpURLConnection.HTTP_INTERNAL_ERROR);
             statusRetorno.setMensagemRetorno(String.valueOf(e.getMessage()));
             statusRetorno.setExtra(e.getLocalizedMessage());
-            
+
             // Adiciona o status
             retornoWebService.statusRetorno = statusRetorno;
-            
+
             return new Gson().toJson(retornoWebService);
         }
     }
-    
+
     @RequestMapping(value = {"/Smadispo"}, method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
-    public String postJson( Model model, 
-                            @RequestHeader() HttpHeaders httpHeaders, 
-                            HttpServletResponse response, 
-                            @RequestParam(name = "dispositivo", required = true) String dispositivo,
-                            @RequestParam(name = "cnpjUrl", required = false) String cnpjUrl,
-                            @RequestParam(name = "columnSelected", required = false) String columnSelected,
-                            @RequestParam(name = "where", required = false) String where,
-                            @RequestParam(name = "resume", required = false, defaultValue = "false") Boolean resume,
-                            @RequestParam(name = "sqlQuery", required = false) String sqlQuery) {
-        
+    public String postJson(Model model,
+            @RequestHeader() HttpHeaders httpHeaders,
+            HttpServletResponse response,
+            @RequestParam(name = "dispositivo", required = true) String dispositivo,
+            @RequestParam(name = "cnpjUrl", required = false) String cnpjUrl,
+            @RequestParam(name = "columnSelected", required = false) String columnSelected,
+            @RequestParam(name = "where", required = false) String where,
+            @RequestParam(name = "resume", required = false, defaultValue = "false") Boolean resume,
+            @RequestParam(name = "sqlQuery", required = false) String sqlQuery) {
+
         StatusRetornoWebServiceBeans statusRetorno = new StatusRetornoWebServiceBeans();
         RetornoWebServiceBeans retornoWebService = new RetornoWebServiceBeans();
         try {
             // Coverte o dispositivo passado no formato json em uma entidade
             SmadispoEntity smadispoEntity = new Gson().fromJson(dispositivo, SmadispoEntity.class);
-            
-            if (smadispoService.findCustomNativeQuery(false, null, null, "IDENTIFICACAO = '" + smadispoEntity.getIdentificacao() + "'").size() < 1) {
-                
-                String insertDispositivo = 
-                          "INSERT INTO SMADISPO(ID_CFACLIFO, DESCRICAO, IDENTIFICACAO) VALUES "
-                        + "((SELECT CFACLIFO.ID_CFACLIFO FROM CFACLIFO WHERE CFACLIFO.CPF_CGC = '" + cnpjUrl + "'), "
-                        + "'" + smadispoEntity.getDescricao() + "', "
-                        + "'" + smadispoEntity.getIdentificacao() + "'"
-                        + " );";
-                Integer qtdInsertDispoLicenca = (Integer) smadispoService.saveCustomNativeQuery(insertDispositivo);
-                // Checa se foi inserido com sucesso
-                if (qtdInsertDispoLicenca > 0){
-                    insertDispositivo = 
-                            "INSERT INTO SMADISPO(DESCRICAO, IDENTIFICACAO) VALUES "
-                          + "('" + smadispoEntity.getDescricao() + "', "
-                          + "'" + smadispoEntity.getIdentificacao() + "'"
-                          + " );";
-                    smadispoService.setSmadispoEntity(smadispoEntity);
-                    
-                    Integer qtdInsertDispo = (Integer)smadispoService.saveCustomNativeQuery(insertDispositivo);
-                    
-                    if (qtdInsertDispo > 0){
-                        // Cria uma vareavel para retorna o status
-                        statusRetorno.setCodigoRetorno(HttpURLConnection.HTTP_OK);
-                        statusRetorno.setMensagemRetorno(String.valueOf(HttpStatus.OK) + "\n" + MensagemPadrao.INSERT_SUCCESS);
 
-                        // Adiciona o status
-                        retornoWebService.statusRetorno = statusRetorno;
-                        // Adiciona os dados que eh pra ser retornado
-                        retornoWebService.object = qtdInsertDispo;
+            if (smadispoService.findCustomNativeQuery(false, null, null, "IDENTIFICACAO = '" + smadispoEntity.getIdentificacao() + "'").size() < 1) {
+
+                CfaclifoEntity cfaclifoEntity = cfaclifoService.findCustomNativeQuery(false, null, null, "(CPF_CGC = '" + cnpjUrl + "')").get(0);
+
+                if (cfaclifoEntity != null) {
+                    String whereChecaQtdLicenca = "ID_CFACLIFO = (SELECT CFACLIFO.ID_CFACLIFO FROM CFACLIFO WHERE  (CPF_CGC = '" + cnpjUrl + "'))";
+
+                    List<SmadispoEntity> listaDispositivo = smadispoService.findCustomNativeQuery(false, null, null, whereChecaQtdLicenca);
+
+                    if ((listaDispositivo != null) && (listaDispositivo.size() < cfaclifoEntity.getQtdeLicencaMovel())) {
+
+                        String insertDispositivo
+                                = "INSERT INTO SMADISPO(ID_CFACLIFO, DESCRICAO, IDENTIFICACAO) VALUES "
+                                + "((SELECT CFACLIFO.ID_CFACLIFO FROM CFACLIFO WHERE CFACLIFO.CPF_CGC = '" + cnpjUrl + "'), "
+                                + "'" + smadispoEntity.getDescricao() + "', "
+                                + "'" + smadispoEntity.getIdentificacao() + "'"
+                                + " );";
+                        Integer qtdInsertDispoLicenca = (Integer) smadispoService.saveCustomNativeQuery(insertDispositivo);
+                        // Checa se foi inserido com sucesso no banco de dados administrativo
+                        if (qtdInsertDispoLicenca > 0) {
+                            insertDispositivo
+                                    = "INSERT INTO SMADISPO(DESCRICAO, IDENTIFICACAO) VALUES "
+                                    + "('" + smadispoEntity.getDescricao() + "', "
+                                    + "'" + smadispoEntity.getIdentificacao() + "'"
+                                    + " );";
+                            smadispoService.setSmadispoEntity(smadispoEntity);
+
+                            Integer qtdInsertDispo = (Integer) smadispoService.saveCustomNativeQuery(insertDispositivo);
+
+                            if (qtdInsertDispo > 0) {
+                                // Cria uma vareavel para retorna o status
+                                statusRetorno.setCodigoRetorno(HttpURLConnection.HTTP_OK);
+                                statusRetorno.setMensagemRetorno(String.valueOf(HttpStatus.OK) + "\n" + MensagemPadrao.INSERT_SUCCESS);
+
+                                // Adiciona o status
+                                retornoWebService.statusRetorno = statusRetorno;
+                                // Adiciona os dados que eh pra ser retornado
+                                retornoWebService.object = qtdInsertDispo;
+                            } else {
+                                // Cria uma vareavel para retorna o status
+                                statusRetorno.setCodigoRetorno(HttpURLConnection.HTTP_INTERNAL_ERROR);
+                                statusRetorno.setMensagemRetorno(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR) + " - Não inseriu no banco de dados do cliente.\n" + MensagemPadrao.INSERT_ERROR);
+
+                                // Adiciona o status
+                                retornoWebService.statusRetorno = statusRetorno;
+                            }
+                        } else {
+                            // Cria uma vareavel para retorna o status
+                            statusRetorno.setCodigoRetorno(HttpURLConnection.HTTP_INTERNAL_ERROR);
+                            statusRetorno.setMensagemRetorno(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR) + " - Inserindo no banco de licença.\n" + MensagemPadrao.INSERT_ERROR);
+
+                            // Adiciona o status
+                            retornoWebService.statusRetorno = statusRetorno;
+                            // Adiciona os dados que eh pra ser retornado
+                            retornoWebService.object = qtdInsertDispoLicenca;
+                        }
                     } else {
                         // Cria uma vareavel para retorna o status
                         statusRetorno.setCodigoRetorno(HttpURLConnection.HTTP_INTERNAL_ERROR);
-                        statusRetorno.setMensagemRetorno(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR) + " - Não inseriu no banco de dados do cliente.\n" + MensagemPadrao.INSERT_ERROR);
+                        statusRetorno.setMensagemRetorno(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR) + " - Este CNPJ não tem mais licença de dispositivo/aparelho disponível.\n" + MensagemPadrao.INSERT_ERROR);
 
                         // Adiciona o status
                         retornoWebService.statusRetorno = statusRetorno;
+
+                        return new Gson().toJson(retornoWebService);
                     }
                 } else {
                     // Cria uma vareavel para retorna o status
                     statusRetorno.setCodigoRetorno(HttpURLConnection.HTTP_INTERNAL_ERROR);
-                    statusRetorno.setMensagemRetorno(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR) + " - Inserindo no banco de licença.\n" + MensagemPadrao.INSERT_ERROR);
+                    statusRetorno.setMensagemRetorno(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR) + " - Este CNPJ não está licenciado e/ou cadastrado.\n" + MensagemPadrao.INSERT_ERROR);
 
                     // Adiciona o status
                     retornoWebService.statusRetorno = statusRetorno;
-                    // Adiciona os dados que eh pra ser retornado
-                    retornoWebService.object = qtdInsertDispoLicenca;
+
+                    return new Gson().toJson(retornoWebService);
                 }
             } else {
                 // Pega o banco de dados do cliente
                 smadispoService.setSmadispoEntity(smadispoEntity);
-                
+
                 if (smadispoService.findCustomNativeQuery(false, null, null, "IDENTIFICACAO = '" + smadispoEntity.getIdentificacao() + "'").size() < 1) {
-                    String insertDispositivo = 
-                            "INSERT INTO SMADISPO(DESCRICAO, IDENTIFICACAO) VALUES "
-                          + "('" + smadispoEntity.getDescricao() + "', "
-                          + "'" + smadispoEntity.getIdentificacao() + "'"
-                          + " );";
-                    
-                    Integer qtdInsertDispo = (Integer)smadispoService.saveCustomNativeQuery(insertDispositivo);
-                    
-                    if (qtdInsertDispo > 0){
+                    String insertDispositivo
+                            = "INSERT INTO SMADISPO(DESCRICAO, IDENTIFICACAO) VALUES "
+                            + "('" + smadispoEntity.getDescricao() + "', "
+                            + "'" + smadispoEntity.getIdentificacao() + "'"
+                            + " );";
+
+                    Integer qtdInsertDispo = (Integer) smadispoService.saveCustomNativeQuery(insertDispositivo);
+
+                    if (qtdInsertDispo > 0) {
                         // Cria uma vareavel para retorna o status
                         statusRetorno.setCodigoRetorno(HttpURLConnection.HTTP_OK);
                         statusRetorno.setMensagemRetorno(String.valueOf(HttpStatus.OK) + "\n" + MensagemPadrao.INSERT_SUCCESS);
@@ -188,29 +222,29 @@ public class SmadispoController extends BaseMyController{
             }
             // Adiciona o status
             retornoWebService.statusRetorno = statusRetorno;
-            
+
             return new Gson().toJson(retornoWebService);
         } catch (JsonSyntaxException e) {
             // Cria uma vareavel para retorna o status
             statusRetorno.setCodigoRetorno(HttpURLConnection.HTTP_INTERNAL_ERROR);
             statusRetorno.setMensagemRetorno(String.valueOf(e.getMessage()));
             statusRetorno.setExtra(e.getLocalizedMessage());
-            
+
             // Adiciona o status
             retornoWebService.statusRetorno = statusRetorno;
-            
+
             return new Gson().toJson(retornoWebService);
-        }  catch (Exception e) {
+        } catch (Exception e) {
             // Cria uma vareavel para retorna o status
             statusRetorno.setCodigoRetorno(HttpURLConnection.HTTP_INTERNAL_ERROR);
             statusRetorno.setMensagemRetorno(String.valueOf(e.getMessage()));
             statusRetorno.setExtra(e.getLocalizedMessage());
-            
+
             // Adiciona o status
             retornoWebService.statusRetorno = statusRetorno;
-            
+
             return new Gson().toJson(retornoWebService);
-        } finally{
+        } finally {
             smadispoService.closeEntityManager();
         }
     }
